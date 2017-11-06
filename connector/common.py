d0da37d7d6549413694ba93412aa56474618cab0
Kevin Zhao <kevin.zhaoshuai@gmail.com>
2017-11-06 18:30:40 +0800

    Add check the allocations and time
    Signed-off-by: Kevin Zhao <kevin.zhaoshuai@gmail.com>

--- connector/common.py
+++ connector/common.py
@@ -19,17 +19,27 @@ def pod_to_capsule(pod):
     pod.api_version = "beta"
     pod_json = pod.to_dict()
     containers = pod_json.get('spec').get('containers')
+
     for container in containers:
         container['environment'] = container.get('env')
         container['workdir'] = container.get('working_dir')
         container['stdin_open'] = container.get('stdin')
+        ##NOTE(kevinz): Here we map the limits to zun "allocation"
+        if container.get('resources').get('limits') is not None:
+            container.get('resources')['allocation'] = \
+                container.get('resources').get('limits')
+        else:
+            container.pop('resources')
         container.pop('env')
         container.pop('working_dir')
         container.pop('stdin')
     creation_time = pod_json.get('metadata').get('creation_timestamp')
     deletion_time = pod_json.get('metadata').get('deletion_timestamp')
-    pod_json.get('metadata')['creation_timestamp'] = \
-        creation_time.strftime("%Y-%m-%d %H:%M:%S%z")
-    pod_json.get('metadata')['deletion_timestamp'] = \
-        deletion_time.strftime("%Y-%m-%d %H:%M:%S%z")
+    if creation_time is not None:
+        pod_json.get('metadata')['creation_timestamp'] = \
+            creation_time.strftime("%Y-%m-%d %H:%M:%S%z")
+    if deletion_time is not None:
+        pod_json.get('metadata')['deletion_timestamp'] = \
+            deletion_time.strftime("%Y-%m-%d %H:%M:%S%z")
+    import pdb;pdb.set_trace()
     return pod_json
